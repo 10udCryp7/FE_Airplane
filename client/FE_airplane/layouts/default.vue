8190ceb4ce53e7025d8d81ce5f2395c1e27506c5
ho1ow <nguyenminhchaucnt@gmail.com>
2024-12-21 12:03:19 +0700

    auth component

--- client/FE_airplane/layouts/default.vue
+++ client/FE_airplane/layouts/default.vue
@@ -1,158 +1,178 @@
 <template>
-  <header class="header">
+  <div class="relative">
+    <header
+      class="absolute top-0 left-0 w-full z-20 flex justify-between items-center p-64 py-4 text-white bg-transparent font-sans"
+    >
       <!-- set logo path -->
-      <div class="logo-container scale-75">
+      <div class="logo-container scale-75 flex items-center">
         <img src="../assets/images/logo2.png" alt="logo" class="logo" />
         <img src="../assets/images/QAirline.png" alt="logo" class="logo" />
       </div>
-      <!-- set header navigation bar -->
-      <nav class="nav">
-          <!-- set DropdownButton for 'Explore' -->
-          <DropdownButton>
-              <template v-slot:title>Explore</template>
-              <template v-slot:content>
-                  <!-- ADD CONTENT HERE -->
-                  <!--DEMO CONTENT BELOW-->
-                  <a href="#">Destinations</a>
-                  <a href="#">Travel Guides</a>
-              </template>
-          </DropdownButton>
-          <!-- set DropdownButton for 'Book' -->
-          <DropdownButton>
-              <template v-slot:title>Book</template>
-              <template v-slot:content>
-                  <!-- ADD CONTENT HERE -->
-                  <!--DEMO CONTENT BELOW-->
-                  <a href="#">Flights</a>
-                  <a href="#">Hotels</a>
-              </template>
-          </DropdownButton>
-          <!-- set DropdownButton for 'Experience' -->
-          <DropdownButton>
-              <template v-slot:title>Experience</template>
-              <template v-slot:content>
-                  <!-- ADD CONTENT HERE -->
-                  <!--DEMO CONTENT BELOW-->
-                  <a href="#">Events</a>
-                  <a href="#">Activities</a>
-              </template>
-          </DropdownButton>
-          <div v-if="userStore.getCurrentUser.isAuthenticated">
-            <button @click="navigateToProfile">Profile</button>
-            <button @click="logout">Logout</button>
-          </div>
 
-          <div v-else>
-            <button @click="navigateToSignIn">Sign In</button>
-            <button @click="navigateToSignUp">Sign Up</button>
-          </div>
+      <!-- set header navigation bar -->
+      <nav class="nav flex space-x-4 text-white gap-5 ml-5">
+        <!-- set DropdownButton for 'Explore' -->
+        <DropdownButton class="text-lg font-light">
+          <template v-slot:title>Explore</template>
+          <template v-slot:content>
+            <!-- ADD CONTENT HERE -->
+            <!--DEMO CONTENT BELOW-->
+            <!-- <a href="#">Destinations</a>
+                    <a href="#">Travel Guides</a> -->
+          </template>
+        </DropdownButton>
+        <!-- set DropdownButton for 'Book' -->
+        <DropdownButton class="text-lg font-light">
+          <template v-slot:title>Book</template>
+          <template v-slot:content>
+            <!-- ADD CONTENT HERE -->
+            <!--DEMO CONTENT BELOW-->
+            <!--                     <a href="#">Flights</a>
+                    <a href="#">Hotels</a> -->
+          </template>
+        </DropdownButton>
+
+        <!-- set DropdownButton for 'Experience' -->
       </nav>
-  </header>   
+      <div
+        class="auth-buttons flex space-x-4 text-white pr-12 font-medium gap-4 ml-auto"
+      >
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+
+        <div v-if="userStore.getAuthenticated">
+          <button>Hello, {{ userStore.getCurrentUser.name }}</button>
+          <button @click="navigateToLogOut">Logout</button>
+        </div>
+
+        <div v-else>
+          <button @click="navigateToSignIn">Sign In</button>
+          <button @click="navigateToSignUp">Sign Up</button>
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
 
-<script >
-import { useRouter } from 'vue-router';
-import { useUserStore } from '~/stores/users';
+<script>
+import { useRouter } from "vue-router";
+import { useUserStore } from "~/stores/users";
 
 export default {
   setup() {
-  const userStore = useUserStore();
-  const router = useRouter();
+    const userStore = useUserStore();
+    const router = useRouter();
 
-  const navigateToSignIn = () => {
-    router.push('/login');
-  }
+    const navigateToSignIn = () => {
+      router.push("/login");
+    };
 
-  const navigateToSignUp = () => {
-    router.push('/register');
-  }
-  return {
+    const navigateToSignUp = () => {
+      router.push("/register");
+    };
+    return {
       userStore,
       navigateToSignIn,
       navigateToSignUp,
     };
-  },  
-}
+  },
+};
 </script>
 
 <style scoped>
-/* Header Styles */
-.header {
-display: grid;
-grid-template-columns: auto 1fr auto; /* Logo, navigation, và phần bên phải */
-align-items: center;
-padding: 10px 20px;
-background-color: rgba(0, 0, 0, 0.7); /* Màu nền trong suốt */
-color: white; /* Màu chữ */
-box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Hiệu ứng đổ bóng */
-}
-
 /* Logo Section */
 .logo {
-max-height: 40px;
+  @apply max-h-10;
 }
 
 /* Navigation Styles */
-.nav {
-display: grid;
-grid-template-columns: repeat(5, auto); /* 5 mục menu */
-justify-content: center;
-gap: 20px; /* Khoảng cách giữa các mục */
-}
+/* .nav {
+  display: grid;
+  grid-template-columns: repeat(5, auto); 
+  justify-content: center;
+  gap: 20px; 
+} */
 
-.nav a {
-text-decoration: none;
-color: white;
-font-weight: 500;
-font-size: 14px;
-padding: 5px 10px;
-border-radius: 5px;
-transition: background-color 0.3s ease, color 0.3s ease;
+/* .nav a {
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+  font-size: 14px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease, color 0.3s ease;
 }
 
 .nav a:hover {
-background-color: rgba(255, 255, 255, 0.2); /* Hiệu ứng nền khi hover */
-color: #f9f9f9;
+  background-color: rgba(255, 255, 255, 0.2); 
+  color: #f9f9f9;
+} */
+
+.nav > DropdownButton {
+  all: unset;
+  @apply text-lg font-medium text-black cursor-pointer transition-colors duration-300;
+}
+
+.nav > DropdownButton:hover {
+  @apply text-opacity-70;
 }
 
 /* Right Section Styles */
-.right-section {
-display: flex;
-align-items: center;
-gap: 15px;
-font-size: 14px;
+/* .right-section {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  font-size: 14px;
 }
 
 .right-section a {
-text-decoration: none;
-color: white;
-transition: color 0.3s ease;
+  text-decoration: none;
+  color: white;
+  transition: color 0.3s ease;
 }
 
 .right-section a:hover {
-color: #f9f9f9;
+  color: #f9f9f9;
 }
 
 .right-section .icon {
-font-size: 16px; /* Icon kích thước nhỏ */
-margin-right: 5px;
+  font-size: 16px; 
+  margin-right: 5px;
 }
 
 .right-section img {
-width: 20px;
-height: 20px;
-border-radius: 50%; /* Icon quốc gia dạng tròn */
+  width: 20px;
+  height: 20px;
+  border-radius: 50%; 
+} */
+
+header {
+  font-family: "Neue Haas Unica Pro", sans-serif;
 }
 
 /* Responsive Styles */
 @media (max-width: 768px) {
-.nav {
-  grid-template-columns: repeat(3, auto); /* Thu gọn khi màn hình nhỏ */
-}
+  .nav {
+    grid-template-columns: repeat(3, auto); /* Thu gọn khi màn hình nhỏ */
+  }
 
-.right-section {
-  gap: 10px;
-  font-size: 12px;
-}
+  .right-section {
+    gap: 10px;
+    font-size: 12px;
+  }
 }
 </style>
