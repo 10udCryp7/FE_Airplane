e7c1d08b55ecfaf7d173c2e43bae37ed8098b862
ho1ow <nguyenminhchaucnt@gmail.com>
2024-12-21 17:46:47 +0700

    add

--- client/FE_airplane/components/admin/InputFlightInfo.vue
+++ client/FE_airplane/components/admin/InputFlightInfo.vue
@@ -1,5 +1,26 @@
 <template>
   <section>
+      <h3>Nhập dữ liệu về các chuyến bay</h3>
+      <h4>Thêm hãng hàng không</h4>
+      <input v-model="flight.airline" placeholder="Hãng hàng không" />
+      <h4>Thêm điểm khởi hành</h4>
+      <input v-model="flight.departure" placeholder="Điểm khởi hành" />
+      <h4>Thêm điểm đến</h4>
+      <input v-model="flight.arrival" placeholder="Điểm đến" />
+      <h4>Thêm thời gian khởi hành</h4>
+      <input
+        type="datetime-local"
+        v-model="flight.departureTime"
+        placeholder="Thời gian khởi hành"
+      />
+      <h4>Thêm thời gian đến</h4>
+      <input
+        type="datetime-local"
+        v-model="flight.arrivalTime"
+        placeholder="Thời gian đến"
+      />
+      <button @click="addFlight">Thêm chuyến bay</button>
+
     <h3>Danh sách các chuyến bay</h3>
     <table border="1">
       <thead>
